a883a3d890fffa3bae1ae6e75d4ff79dbf8b68c0
Merrick <merrickneo@gmail.com>
2023-07-12 15:05:50 +0800

    Update variables and outputs tf files

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "remote" {
+        organization = "hashicorp-learn"
+        workspaces {
+            name = "My-Testing-Workspace"
+        }
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -18,7 +24,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
 
