27949c9391bf3b1ccd0331b7d45571ebd41d288e
Merrick <merrickneo@gmail.com>
2023-07-13 14:31:52 +0800

    Add terraform scripts to create Thing Groups & Types

--- main.tf
+++ main.tf
@@ -1,31 +1,55 @@
 terraform {
-    backend "remote" {
-        organization = "hashicorp-learn"
-        workspaces {
-            name = "My-Testing-Workspace"
-        }
-    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-southeast-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
+resource "aws_iot_thing_group" "office_group" {
+  name = "office-group"
+  properties {
+    attribute_payload {
+      attributes = {
+        location = "NCS"
+      }
+    }
   }
 }
 
+resource "aws_iot_thing_group" "robots" {
+  name              = "robots"
+  parent_group_name = aws_iot_thing_group.office_group.name
+}
 
+resource "aws_iot_thing_group" "cctv-camera" {
+  name              = "cctv-camera"
+  parent_group_name = aws_iot_thing_group.office_group.name
+}
+
+resource "aws_iot_thing_type" "cleaning_robot" {
+  name = "cleaning_robot"
+  properties {
+    searchable_attributes = ["Mopping"]
+  }
+}
+
+resource "aws_iot_thing_type" "surveillance_robot" {
+  name = "surveillance_robot"
+  properties {
+    searchable_attributes = ["Alarm"]
+  }
+}
+
+resource "aws_iot_thing_type" "lift-camera" {
+    name = "lift-camera"
+    properties {
+      searchable_attributes = ["24/7"]
+    }
+}
